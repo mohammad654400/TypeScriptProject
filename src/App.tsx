5721b0d092aced124730e035bf93cff8ab8f2e5c
arian_shahalami <arian_shahalami@outlook.com>
2024-05-06 23:30:54 +0330

    javad joonam

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,102 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Elements from "./elements";
 import ViewElements from "./view-elements";
 import Details from "./details";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Container, Row } from "react-bootstrap";
-import { ApiItem, baseType } from "./types";
+import {Col, Container, Row} from "react-bootstrap";
+import {ApiItem, baseType} from "./types";
 import axios from "axios";
 
 function App() {
-  const [selectedItemId, setSelectedItemId] = useState<string>("");
-  const [elementList, setElementList] = useState<baseType[]>([]);
-  const [dataItemSelect, setDataItemSelect] = useState<baseType>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [getApi, setGetApi] = useState<ApiItem[]>([]);
-  const baseURL =
-    "http://universities.hipolabs.com/search?country=United+States";
+    const [selectedItemId, setSelectedItemId] = useState<string>("");
+    const [elementList, setElementList] = useState<baseType[]>([]);
+    const [dataItemSelect, setDataItemSelect] = useState<baseType>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [getApi, setGetApi] = useState<ApiItem[]>([]);
+    const baseURL =
+        "http://universities.hipolabs.com/search?country=United+States";
 
-  useEffect(() => {
-    axios
-      .get(baseURL)
-      .then((response) => {
-        const firstTenItems = response.data.slice(0, 10);
-        setGetApi(firstTenItems);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get(baseURL)
+            .then((response) => {
+                const firstTenItems = response.data.slice(0, 10);
+                setGetApi(firstTenItems);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            });
+    }, []);
 
-  const generateRandomId = (type: string) => {
-    return `${type}-${Math.random() * 1000}`;
-  };
-
-  const elementBaseData = {
-    width: 6,
-    height: 6,
-    title: "hi",
-    order: elementList.length + 1,
-  };
-
-  const addItemToList = (type: string) => {
-    const newItem: baseType = {
-      id: generateRandomId(type),
-      type: type,
-      ...elementBaseData,
+    const generateRandomId = (type: string) => {
+        return `${type}-${Math.random() * 1000}`;
     };
-    setElementList([...elementList, newItem]);
-  };
 
-  const updateList = (newData: Partial<baseType>, itemId: string) => {
-    if (itemId && newData) {
-      const updatedList = elementList.map((item) =>
-        item.id === itemId ? { ...item, ...newData } : item
-      );
+    const elementBaseData = {
+        width: 6,
+        height: 6,
+        title: "hi",
+        order: elementList.length + 1,
+    };
 
-      updatedList.sort(function (a, b) {
-        return a.order - b.order;
-      });
+    const addItemToList = (type: string) => {
+        const newItem: baseType = {
+            id: generateRandomId(type),
+            type: type,
+            ...elementBaseData,
+        };
 
-      setElementList(updatedList);
-    }
-  };
-  console.log("elementList", elementList);
+        switch (type) {
+            case "select":
+                setElementList(prev => [...prev, {...newItem, selectItem: []}]);
+        }
 
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <Elements getType={addItemToList} />
-        </Col>
-        <Col>
-          <ViewElements
-            viewData={elementList}
-            setSelectedItem={setSelectedItemId}
-            setDataItemSelect={setDataItemSelect}
-            selectedItemId={selectedItemId}
-          />
-        </Col>
-        <Col>
-          <Details
-            updateList={updateList}
-            dataItemSelect={dataItemSelect}
-            getApi={getApi}
-            loading={loading}
-            selectedItemId={selectedItemId}
-          />
-        </Col>
-      </Row>
-    </Container>
-  );
+
+    };
+
+    const updateList = (newData: Partial<baseType>, itemId: string) => {
+        if (itemId && newData) {
+            const updatedList = elementList.map((item) =>
+                item.id === itemId ? {...item, ...newData} : item
+            );
+
+            updatedList.sort(function (a, b) {
+                return a.order - b.order;
+            });
+
+            setElementList(updatedList);
+        }
+    };
+    console.log("elementList", elementList);
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Elements getType={addItemToList}/>
+                </Col>
+                <Col>
+                    <ViewElements
+                        viewData={elementList}
+                        setSelectedItem={setSelectedItemId}
+                        setDataItemSelect={setDataItemSelect}
+                        selectedItemId={selectedItemId}
+                    />
+                </Col>
+                <Col>
+                    <Details
+                        updateList={updateList}
+                        dataItemSelect={dataItemSelect}
+                        getApi={getApi}
+                        loading={loading}
+                        selectedItemId={selectedItemId}
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
+
 export default App;
--- src/view-elements.tsx
+++ src/view-elements.tsx
@@ -1,96 +1,84 @@
-import { baseType } from "./types";
+import {baseType} from "./types";
 
 const ViewElements = ({
-  viewData,
-  setSelectedItem,
-  setDataItemSelect,
-  selectedItemId
-}: {
-  viewData: baseType[];
-  setSelectedItem: (id: string) => void;
-  setDataItemSelect: (item: baseType) => void;
-  selectedItemId:string
+                          viewData,
+                          setSelectedItem,
+                          setDataItemSelect,
+                          selectedItemId
+                      }: {
+    viewData: baseType[];
+    setSelectedItem: (id: string) => void;
+    setDataItemSelect: (item: baseType) => void;
+    selectedItemId: string
 }) => {
-  interface RenderOptions {
-    [key: string]: (
-      id: string,
-      title: string,
-      width: number,
-      height: number,
-      fontSize?: number,
-      selectItem?: string[]
-    ) => JSX.Element;
-  }
-  console.log("viewData", viewData);
+    interface RenderOptions {
+        [key: string]: (
+            id: string,
+            title: string,
+            width: number,
+            height: number,
+            fontSize?: number,
+            selectItem?: string[]
+        ) => JSX.Element;
+    }
+
+    console.log("viewData", viewData);
 
 
-  const renderOptions: RenderOptions = {
-    text: (id, title, width, height, fontSize) => (
-      <h1 style={{ fontSize: `${fontSize}px` }}>{title}</h1>
-    ),
-    button: (id, title, width, height) => (
-      <button style={{ width: "100%", height: "100%" }}>{title}</button>
-    ),
-    select: (id, title, width, height) => (
-      
-      <select>
-      {viewData.map((x) => {
-        if (x.id === selectedItemId) {
-          return (
-            <option key={x.id} value={x.selectItem}>
-              {x.selectItem}
-            </option>
-          );
-        }
-      })}
-    </select>
-    
-    ),
-  };
+    const renderOptions: RenderOptions = {
+        text: (id, title, width, height, fontSize) => (
+            <h1 style={{fontSize: `${fontSize}px`}}>{title}</h1>
+        ),
+        button: (id, title, width, height) => (
+            <button style={{width: "100%", height: "100%"}}>{title}</button>
+        ),
+        select: ({id, title, selectItem}) => (
+            <select>
+                {selectItem.map(item =>
+                    <option>{item}</option>
+                )}
+            </select>
 
-  const handleItemClick = (item: baseType) => {
-    setSelectedItem(item.id);
-    setDataItemSelect(item);
- 
-  };
+        ),
+    };
+
+    const handleItemClick = (item: baseType) => {
+        setSelectedItem(item.id);
+        setDataItemSelect(item);
+
+    };
 
 
-
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(24,1fr)",
-        gridTemplateRows: "repeat(24,1fr)",
-        gridGap: "10px",
-        backgroundColor: "green",
-      }}
-    >
-      {viewData.map((item) => (
+    console.log("viewDatta", viewData);
+    return (
         <div
-          key={item.id}
-          onClick={() => handleItemClick(item)}
-          style={{
-            gridColumnEnd: `span ${item.width}`,
-            gridRowEnd: `span ${item.height}`,
-            color: `${item?.color}`,
-            display: item?.display,
-            justifyContent: item?.justifyContent,
-            alignItems: item?.alignItem,
-          }}
+            style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(24,1fr)",
+                gridTemplateRows: "repeat(24,1fr)",
+                gridGap: "10px",
+                backgroundColor: "green",
+            }}
         >
-          {item.type in renderOptions &&
-            renderOptions[item.type](
-              item.id,
-              item.title,
-              item.width,
-              item.height,
-              item?.fontSize,
-            )}
+            {viewData.map((item) => (
+                <div
+                    key={item.id}
+                    onClick={() => handleItemClick(item)}
+                    style={{
+                        gridColumnEnd: `span ${item.width}`,
+                        gridRowEnd: `span ${item.height}`,
+                        color: `${item?.color}`,
+                        display: item?.display,
+                        justifyContent: item?.justifyContent,
+                        alignItems: item?.alignItem,
+                    }}
+                >
+                    {item.type in renderOptions &&
+                        renderOptions[item.type]({id: item?.id, title: item?.title, selectItem: item?.selectItem})}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default ViewElements;
